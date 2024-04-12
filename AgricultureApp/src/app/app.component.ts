cdd917e9e5557f1561bd53f055b2d7c2d70f44e7
devarshi-patidar1812 <devarshi330@gmail.com>
2024-04-12 23:38:41 +0530

    Created Common Component

--- AgricultureApp/src/app/app.component.ts
+++ AgricultureApp/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './common/footer/footer.component';
+import { HeaderComponent } from './common/header/header.component';
+import { HomeComponent } from './common/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

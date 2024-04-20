91b79ff530481f2514c3d91db6722d996b264280
devarshi-patidar1812 <devarshi330@gmail.com>
2024-04-20 18:23:11 +0530

    Update app.routes.ts

--- AgricultureApp/src/app/app.routes.ts
+++ AgricultureApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MandiBhaavComponent } from './mandi-bhaav/mandi-bhaav.component';
+import { HomeComponent } from './common/home/home.component';
+import { SettingsComponent } from './common/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',   redirectTo: '/home', pathMatch: 'full' },
+    {path:'home',component:HomeComponent},
+    {path:'mandibhaav' , component: MandiBhaavComponent},
+    {path:'settings',component:SettingsComponent}
+]

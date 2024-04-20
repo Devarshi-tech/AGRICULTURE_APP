a484fe7e6653a4c0acfd7e4f3ff79120d1b17849
devarshi-patidar1812 <devarshi330@gmail.com>
2024-04-20 18:23:55 +0530

    Updated App component

--- AgricultureApp/src/app/app.component.ts
+++ AgricultureApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './common/footer/footer.component';
 import { HeaderComponent } from './common/header/header.component';
@@ -7,10 +7,32 @@ import { HomeComponent } from './common/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,HeaderComponent,HomeComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AgricultureApp';
+
+  public getScreenWidth: any;
+  getScreenHeight: any;
+
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      // Access window here
+      this.getScreenWidth = window.innerWidth;
+      this.getScreenHeight = window.innerHeight;
+    }
+
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    if (typeof window !== 'undefined') {
+      // Access window here
+      this.getScreenWidth = window.innerWidth;
+      this.getScreenHeight = window.innerHeight;
+    }
+  }
+
 }

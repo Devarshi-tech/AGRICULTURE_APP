2120946af87eadf76ec71dbd8dca2169409ceb45
Pritam patidar <pritampatidar29@gmail.com>
2024-04-28 22:55:48 +0530

    weather component added

--- AgricultureApp/src/app/common/home/home.component.ts
+++ AgricultureApp/src/app/common/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { WeatherComponent } from '../weather/weather.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,WeatherComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
